1d089d3130d0880d7ea604ca439c191b2ac35dd1
Rachitshukla12 <127474161+Rachitshukla12@users.noreply.github.com>
2025-04-28 17:45:06 +0000

    Changes

---
+++ 01_basics/05_dates.js
@@ -0,0 +1,9 @@
+// Dates
+
+let MyDate = new Date();
+console.log(MyDate);
+console.log(MyDate.toString());
+console.log(MyDate.toDateString());
+console.log(MyDate.toJSON());
+console.log(MyDate.toISOString());
+console.log(MyDate.toLocaleString());
