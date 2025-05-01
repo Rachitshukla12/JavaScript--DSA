5fddc32706a8e46eb2fc5a15a169be3e153dc7ab
Rachitshukla12 <rachit.shukla098@gmail.com>
2025-05-01 12:52:33 -0400

    Forloop

---
+++ 02_basics/02_forloop.js
@@ -0,0 +1,18 @@
+// for (let i =1; i<=5; i++){
+//     console.log(i)
+// }
+// Output: 1, 2, 3, 4, 5
+
+                     ///Odd numbers &Even numbers///
+
+// for (let i =1; i<=20; i+=2){
+//     console.log(i)
+// } // Odd numbers from 1 to 20
+
+// for (let i=2; i<=10; i=i+2){
+//     console.log(i);    
+// }// Even numbers from 2 to 10
+
+// for (let i=10; i>=2; i=i-2){ //Parameter is changed from above//
+//     console.log(i);
+// }//Backward even numbers from 10 to 2
